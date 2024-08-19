df55e8c0fbc899e2bc84963e14a0d210342cfd39
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-08-19 15:49:28 +0700

    Module7-task1 (#8)
    * Сетки секций целей, шагов
    
    * Сетки header, hero. добалвено событие принажатии кнопки
    
    * рефакторинг
    
    * Живой пример
    
    * сетки каталога
    
    * дополнительные товары
    
    * subscribe
    
    * рефакторинг
    
    * рефакторинг
    
    * Карта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
-/* в этот файл добавляет скрипты*/
+const navMobile = document.querySelector('.page-header_nav');
+const buttonToggle = document.querySelector('.page-header__nav-toggle');
+
+navMobile.classList.remove('page-header_nav--state-open');
+buttonToggle.classList.remove('page-header__nav-toggle--cross');
+buttonToggle.classList.add('page-header__nav-toggle--burger');
+
+
+buttonToggle.onclick = function() {
+  navMobile.classList.toggle('page-header_nav--state-open');
+  if (buttonToggle.classList.contains('page-header__nav-toggle--cross')) {
+    buttonToggle.classList.remove('page-header__nav-toggle--cross');
+    buttonToggle.classList.add('page-header__nav-toggle--burger');
+  }else{
+    buttonToggle.classList.add('page-header__nav-toggle--cross');
+  }
+};
