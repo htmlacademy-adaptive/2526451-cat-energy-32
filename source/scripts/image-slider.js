5b8323f8649c3dad877ef8c88396bc63484bab80
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-09-06 15:20:16 +0700

    js

--- source/scripts/image-slider.js
+++ source/scripts/image-slider.js
@@ -33,6 +33,38 @@ toggle.addEventListener('touchstart', (event) => {
     isDraging = false;
   }
 });
+
+
+toggle.onmousedown = function (event) {
+  event.preventDefault();
+  const shiftX = event.clientX - toggle.getBoundingClientRect().left;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+  function onMouseMove (e) {
+    let newLeft = e.clientX - shiftX - slider.getBoundingClientRect().left + (toggle.getBoundingClientRect().width / 2);
+
+    if(newLeft < 0) {
+      newLeft = 0;
+    }
+
+    if (newLeft > slider.offsetWidth) {
+      newLeft = slider.offsetWidth;
+    }
+
+    newLeft = (newLeft / slider.getBoundingClientRect().width) * 100;
+
+    toggle.style.left = `${newLeft}%`;
+    stick.style.left = `${newLeft}%`;
+    imageBefore.style.width = `${newLeft}%`;
+    imageAfter.style.width = `${100 - newLeft}%`;
+  }
+
+  function onMouseUp () {
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+};
 toggle.ondragstart = function () {
   return false;
 };
